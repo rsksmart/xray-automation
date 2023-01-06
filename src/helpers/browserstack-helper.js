b0746491d6ce6f4e10f3c3bdfd3de144ed269a3b
Javier del Olmo <javier.delolmo@iovlabs.org>
2023-01-06 16:01:32 +0000

    bs-public-link & xray updateTestRunComment

---
+++ src/helpers/browserstack-helper.js
@@ -0,0 +1,21 @@
+import XrayErrorResponse from '../xray-error-response.js';
+import BrowserstackSession from '../browserstack-session.js';
+
+export async function getSessionPublicLink(sessionID) {     
+    
+    const browserstackSession = new BrowserstackSession();
+
+    return browserstackSession.getSessionPublicLink(sessionID)
+        .catch( function(error) {
+            if (error.response !== undefined)
+                throw new XrayErrorResponse(error.response);
+            else if (error.body !== undefined)
+                throw new Error(error.body.error);
+            else
+                throw new Error(error.message || error._response);  
+        });
+}
+
+
+const publicLink = await getSessionPublicLink("f2b08e6a82dff962b9dd450b736392fd8589b887");
+console.log(publicLink);
